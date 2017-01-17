c3dfe085c5ab87c858ad325297474931c92c7861
Amir Keren <amirkeren3@gmail.com>
2017-01-17 15:57:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
+const DATABASE_ERROR = 'Database undefined';
+const PARAMETER_ERROR = 'Parameter is missing';
+
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
@@ -12,11 +15,19 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+// bodyParser provides access to the query request parameters
+var bodyParser = require('body-parser');
+
 // create a new express server
 var app = express();
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+
+//for parsing request parameters
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
@@ -25,4 +36,16 @@ var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});
+
+//endpoint for retrieving all students
+app.get('/getStudents', function(req, res) {
+  //TODO - implement
+  res.sendStatus(500);
+});
+
+//endpoint for adding a new student
+app.post('/addStudent', function(req, res) {
+  //TODO - implement
+  res.sendStatus(500);
 });
